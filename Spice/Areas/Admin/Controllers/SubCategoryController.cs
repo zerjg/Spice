33ff57e2ee2d9e055f33fcfa3c40f652e777480c
Игорь Пономарев <igor_p@live.ru>
2020-11-02 15:07:33 +0300

    SubCategory - Add Create post action

--- Spice/Areas/Admin/Controllers/SubCategoryController.cs
+++ Spice/Areas/Admin/Controllers/SubCategoryController.cs
@@ -35,5 +35,32 @@ namespace Spice.Areas.Admin.Controllers
             };
             return View(model);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(SubCategoryAndCategoryViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var doesSubCategoryExists = _db.SubCategory.Include(s => s.Category).Where(s => s.Name == model.SubCategory.Name && s.Category.Id == model.SubCategory.CategoryId);
+                if (doesSubCategoryExists.Count() > 0)
+                {
+                    //error
+                }
+                else
+                {
+                    _db.SubCategory.Add(model.SubCategory);
+                    await _db.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            SubCategoryAndCategoryViewModel modelVM = new SubCategoryAndCategoryViewModel()
+            {
+                CategoryList = await _db.Category.ToListAsync(),
+                SubCategory = model.SubCategory,
+                SubCategoryList = await _db.SubCategory.OrderBy(p => p.Name).Select(p => p.Name).ToListAsync()
+            };
+            return View(modelVM);
+        }
     }
 }
